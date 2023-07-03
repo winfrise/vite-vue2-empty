d0d5e9cb6eb602213b9300a481cd2f5069d2847e
DESKTOP-O2KNQPR\DFL <fanwenxiang326@163.com>
2023-07-03 14:32:38 +0800

    vue2.7需要把vite-plugin-vue2替换为@vitejs/plugin-vue2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue2'
 import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [
-        createVuePlugin()
+        vue()
     ],
     resolve: {
       alias: [
